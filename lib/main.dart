64e46de9a948164d26efe7cd990b01c96c56425e
Ratul Hasan <66786636+Onnesok@users.noreply.github.com>
2024-11-20 20:50:08 +0600

    updated main

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:robu/custom_drawer/navigation_home_screen.dart';
+import 'package:robu/login/login.dart';
 import 'package:robu/user_data/profile_provider.dart';
 import 'themes/app_theme.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await SystemChrome.setPreferredOrientations(<DeviceOrientation>[
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown
-  ]).then((_) => runApp(const MyApp()));
+
+  // Ensure the app only runs in portrait mode
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  // Load SharedPreferences before running the app
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+
+  runApp(MyApp(isLoggedIn: isLoggedIn));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool isLoggedIn;
+  const MyApp({super.key, required this.isLoggedIn});
 
   @override
   Widget build(BuildContext context) {
@@ -34,21 +42,34 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (_) => ProfileProvider()),
       ],
-      child: MaterialApp(
-        title: 'Robu',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          textTheme: AppTheme.textTheme,
-          platform: TargetPlatform.iOS,
-        ),
-        home: const Scaffold(
-          body: SafeArea(
-            top: false,
-            bottom: false,
-            child: NavigationHomeScreen(),
-          ),
-        ),
+      child: Consumer<ProfileProvider>(
+        builder: (context, profileProvider, child) {
+          // Check if the user is logged in
+          return MaterialApp(
+            title: 'Robu',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+              textTheme: AppTheme.textTheme,
+              platform: TargetPlatform.iOS,
+            ),
+            home: profileProvider.isLoggedIn
+                ? const Scaffold(
+              body: SafeArea(
+                top: false,
+                bottom: false,
+                child: NavigationHomeScreen(),
+              ),
+            )
+                : const Scaffold(
+              body: SafeArea(
+                top: false,
+                bottom: false,
+                child: login(),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
